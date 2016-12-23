a820dfbb982114076a80d23b84467b1817747c79
Stevan Koprivica <stevan.koprivica@gmail.com>
2016-12-23 22:47:09 +0100

    fr

---
+++ src/locale/fr.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'days' => ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'],
+	'days3' => ['dim', 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam'],
+	'months' => ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
+	'months3' => ['jan', 'fév', 'mar', 'avr', 'mai', 'juin', 'juil', 'aoû', 'sep', 'oct', 'nov', 'déc'],
+);
+
--- src/locale/sr.php
+++ src/locale/sr.php
@@ -1,10 +1,10 @@
 <?php
 return array(
-		'days' => ['nedelja', 'ponedeljak', 'utorak', 'sreda', 'četvrtak', 'petak', 'subota'],
-		'days3' => ['ned', 'pon', 'uto', 'sre', 'čet', 'pet', 'sub'],
-		'months' => [
-			'januar', 'februar', 'mart', 'april', 'maj', 'jun', 'jul', 'avgust', 'septembar', 'oktobar', 'novembra', 'decembar',
-			'januara', 'februara', 'marta', 'aprila', 'maja', 'juna', 'jula', 'avgusta', 'septembra', 'oktobra', 'novembra', 'decembra',
-		],
-		'months3' => ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'avg', 'sep', 'okt', 'nov', 'dec'],
+	'days' => ['nedelja', 'ponedeljak', 'utorak', 'sreda', 'četvrtak', 'petak', 'subota'],
+	'days3' => ['ned', 'pon', 'uto', 'sre', 'čet', 'pet', 'sub'],
+	'months' => [
+		'januar', 'februar', 'mart', 'april', 'maj', 'jun', 'jul', 'avgust', 'septembar', 'oktobar', 'novembra', 'decembar',
+		'januara', 'februara', 'marta', 'aprila', 'maja', 'juna', 'jula', 'avgusta', 'septembra', 'oktobra', 'novembra', 'decembra',
+	],
+	'months3' => ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'avg', 'sep', 'okt', 'nov', 'dec'],
 );
